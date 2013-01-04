cdc721d54df05356f334d147c8f40f232ebe451c
Lukas Mai <l.mai@web.de>
2013-01-04 03:41:40 +0100

    update copyright date

--- lib/Worlogog/Restart/Restart.pm
+++ lib/Worlogog/Restart/Restart.pm
@@ -16,3 +16,27 @@ for (qw(name code)) {
 'ok'
 
 __END__
+
+=head1 NAME
+
+Worlogog::Restart::Restart - restart objects (internal class)
+
+=head1 DESCRIPTION
+
+See L<C<find> in C<Worlogog::Restart>|Worlogog::Restart/find-RESTART>.
+
+=head1 AUTHOR
+
+Lukas Mai, C<< <l.mai at web.de> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2013 Lukas Mai.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
